be6b21d64e80cf0e46ad3cbf5422348dcdfe9b56
vanillv <vanillvanish@gmail.com>
2024-10-08 21:15:06 +0300

    i fixed a couple of problems again

--- searchengine/src/main/java/searchenginepackage/Application.java
+++ searchengine/src/main/java/searchenginepackage/Application.java
@@ -2,10 +2,11 @@ package searchenginepackage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@SpringBootApplication
+//
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
